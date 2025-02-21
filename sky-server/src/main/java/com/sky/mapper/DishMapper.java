61f4008a5c024f257c483dc0b5eec6cd0d5fc502
Franzly <1029868512@qq.com>
2025-02-21 14:51:15 -0500

    query dish info by pages

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,11 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -23,4 +26,12 @@ public interface DishMapper {
      */
     @AutoFill(value = OperationType.INSERT)
     void insertDish(Dish dish);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    // select d.*, c.name as categoryName from dish d left outer join category c on d.category_id = c.id
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
