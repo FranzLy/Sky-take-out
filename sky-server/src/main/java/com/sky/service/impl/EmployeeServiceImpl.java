1fe800aba77a86a146a1921bb7d9937a68eb40fa
Franzly <1029868512@qq.com>
2025-02-20 23:17:06 -0500

    query employee info by pages

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,22 @@
 package com.sky.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -80,6 +86,23 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
+    }
+
+    @Override
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
+        PageResult pageResult = new PageResult();
+
+        //调用mapper接口查询
+        Page<Employee> pageInfo = new Page(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+        LambdaQueryWrapper<Employee> lqw = new LambdaQueryWrapper<>();
+        lqw.like(StringUtils.isNotEmpty(employeePageQueryDTO.getName()), Employee::getUsername, employeePageQueryDTO.getName());
+        employeeMapper.selectPage(pageInfo, lqw);
+
+        //设置分页信息到结果中
+        //注意，如果不添加mybatis-plus的拦截器，获取到的total值为0，加了拦截器后要清理缓存再重启IDEA才生效
+        pageResult.setTotal(pageInfo.getTotal());
+        pageResult.setRecords(pageInfo.getRecords());
+        return pageResult;
     }
 
 }
