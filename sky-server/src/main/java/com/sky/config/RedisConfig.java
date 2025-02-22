e076ef753a5dcfc2ece72c839b7cbc681949065f
Franzly <1029868512@qq.com>
2025-02-21 22:04:15 -0500

    add redis config and cache shop status

---
+++ sky-server/src/main/java/com/sky/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.sky.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+
+        //默认key序列化器为JdkSerializationRedisSerializer
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setConnectionFactory(factory);
+
+        return template;
+    }
+}
