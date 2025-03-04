e557f5209e78430d0fc999e9e7de205ffef33ae5
Franzly <1029868512@qq.com>
2025-03-04 15:53:35 -0500

    notify that there is a new order at server when user pay successfully.

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -14,6 +14,7 @@ import com.sky.service.OrderService;
 import com.sky.utils.WeChatPayUtil;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +25,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -42,6 +45,8 @@ public class OrderServiceImpl implements OrderService {
     private UserMapper userMapper;
     @Autowired
     private WeChatPayUtil weChatPayUtil;
+    @Autowired
+    private WebSocketServer webSocketServer;
 
     /**
      * 用户下单
@@ -144,6 +149,15 @@ public class OrderServiceImpl implements OrderService {
                 .build();
 
         orderMapper.update(orders);
+
+        //通过websocket 向客户端浏览器推送消息 type orderId content
+        Map map = new HashMap();
+        map.put("type", 1);
+        map.put("orderId", ordersDB.getId());
+        map.put("content", "订单号" + outTradeNo);
+        String json = JSONObject.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
+
     }
 
     /**
--- sky-server/src/main/java/com/sky/task/CronTask.java
+++ sky-server/src/main/java/com/sky/task/CronTask.java
@@ -1,18 +1,26 @@
 package com.sky.task;
 
+import com.alibaba.fastjson.JSONObject;
 import com.sky.service.OrderService;
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Slf4j
 public class CronTask {
     @Autowired
     private OrderService orderService;
+    @Autowired
+    private WebSocketServer webSocketServer;
+
 
     /**
      * 测试spring task
@@ -22,12 +30,36 @@ public class CronTask {
 //        log.info("cronTask");
 //    }
 
+    /**
+     * 通过WebSocket每隔5秒向客户端发送消息
+     */
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void every5SecTask() {
+        log.info("每5秒钟的定时任务：{}", LocalDateTime.now());
+
+        webSocketServer.sendToAllClient("这是来自服务端的消息：" + DateTimeFormatter.ofPattern("HH:mm:ss").format(LocalDateTime.now()));
+    }
+
     @Scheduled(cron = "0 * * * * *")
     public void everyMintueCronTask() {
         log.info("每分钟的定时任务:{}", LocalDateTime.now());
 
         // 处理超时订单
         orderService.processTimeOutOrder();
+    }
+
+    @Scheduled(cron = "0 0 0/1 * * ?")
+    public void everyHourCronTask(){
+        log.info("每小时触发:{}", LocalDateTime.now());
+
+
+        //因为实际上没法调用微信支付成功，所以在这里模拟一下支付成功，推送来单提醒的代码
+        Map map = new HashMap();
+        map.put("type", 1);
+        map.put("orderId", 23);
+        map.put("content", "订单号" +16715906178450L);
+        String json = JSONObject.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
     }
 
     /**
